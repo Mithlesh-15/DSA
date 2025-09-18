d8168fb29e086130685987c6f7077ccd14f1ff46
Mithlesh Kumar Dewangan <kamaldewangan367@gmail.com>
2025-09-18 09:00:32 +0530

    solved 31. Next Permutation

---
+++ LEETCODE/31. Next Permutation.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int pivot = -1, n = nums.size();
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                pivot = i;
+                break;
+            }
+        }
+        if (pivot == -1)
+        {
+            for (int i = 0, j = n - 1; i <= j; i++, j--)
+            {
+                swap(nums[i], nums[j]);
+            }
+            return;
+        }
+        for (int i = n - 1; i > pivot; i--)
+        {
+            if (nums[i] > nums[pivot])
+            {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+        for (int i = pivot + 1, j = n - 1; i <= j; i++, j--)
+        {
+            swap(nums[i], nums[j]);
+        }
+    }
+};
