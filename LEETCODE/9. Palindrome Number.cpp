0cd369aa4f1a381c5fbeb5a3991743325ce328f6
Mithlesh Kumar Dewangan <kamaldewangan367@gmail.com>
2025-09-23 17:53:25 +0530

    solved 9. Palindrome Number

---
+++ LEETCODE/9. Palindrome Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int reverse(int x){
+     int reverseX =  0;
+        while(x!=0){
+            int dig = x%10;
+            if(INT_MAX/10 < reverseX || INT_MIN/10 > reverseX){
+                return 0;
+            }
+            reverseX = (reverseX*10)+dig;
+            x /=10;
+        }
+        return reverseX;
+}
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        return x==reverse(x);
+    }
+};
