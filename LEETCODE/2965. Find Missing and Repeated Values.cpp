9eff2d15fe3a568efbc4efda7ac017b6c10cbb00
Mithlesh Kumar Dewangan <kamaldewangan367@gmail.com>
2025-09-27 11:16:06 +0530

    solved 2965. Find Missing and Repeated Values

---
+++ LEETCODE/2965. Find Missing and Repeated Values.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int> ans;
+        unordered_set<int> s;
+        int n = grid.size();
+        int a,b;
+        int actualSum = 0,expectedSum;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(s.find(grid[i][j]) != s.end()){
+                    a = grid[i][j];
+                    ans.push_back(a);
+                }
+                s.insert(grid[i][j]);
+            }
+        }
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                actualSum += grid[i][j];
+            }
+        }
+        expectedSum = (n*n)*(n*n + 1)/2;
+        b = expectedSum + a - actualSum;
+        ans.push_back(b);
+        return ans;
+    }
+};
