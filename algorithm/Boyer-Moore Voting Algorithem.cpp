94d00f4692e4fa0e0aa9c1b202ffc0e3508e72d5
Mithlesh Kumar Dewangan <kamaldewangan367@gmail.com>
2025-10-24 13:04:09 +0530

    Implement Boyer-Moore Voting Algorithm for majority element I

--- algorithm/Boyer-Moore Voting Algorithem.cpp
+++ algorithm/Boyer-Moore Voting Algorithem.cpp
@@ -1,6 +1,31 @@
 // Boyer-Moore Voting Algorithem
 
 
+
+// 169. Majority Element I - LeetCode
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0;
+        int ans = nums[0];
+        for(int i=0 ;i<nums.size();i++){
+            if(freq==0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+            
+        }
+        return ans;
+    }
+};
+
+
+
+
 // 229. Majority Element II - LeetCode
 class Solution {
 public:
